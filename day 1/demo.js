81c8cde3f5348695ae8096039e37f2a9f3317f1f
Alvin Ng <alvin.cl.ng@gmail.com>
2025-05-15 10:07:40 +0900

    Was able to remove items from the process array

--- day 1/demo.js
+++ day 1/demo.js
@@ -1,1 +1,13 @@
-console.log('hello!')
+// Write a program that takes in an unlimited number of command line arguments and prints out the sum of them. If any argument is not a whole number, skip it. Do not support negative numbers.
+
+// Read the command line
+// console.log(process.argv)
+
+// Make Variables
+// const are values that do not change and will always stay the same
+const argument = process.argv.slice(2);
+
+
+// Test early and TEST EVERYTHING
+console.log(argument);
+
